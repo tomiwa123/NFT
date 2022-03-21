1185a22ecf87a1ca55c1120d136556136b1678f8
Kento Sugama <kentosugama@protonmail.ch>
2022-03-21 10:08:55 -0400

    typo

--- Step3/nft_website/views.py
+++ Step3/nft_website/views.py
@@ -10,7 +10,7 @@ def serve_image(request):
     # 1. Query the algorand blockchain for your NFT
     # 2. Recover the IPFS link from the NFT
     # 3. Query the image from IPFS
-    # 4. Serve the immage as an HTTP response
+    # 4. Serve the image as an HTTP response
 
     image_bytes = open("./beautiful_temple.jpeg", "rb")
     return HttpResponse(image_bytes.read(), content_type="image/jpeg")
