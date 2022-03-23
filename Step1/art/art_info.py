6a07b9a1b89d76ef70a3aff93fb16433a2039f70
Kento Sugama <kentosugama@protonmail.ch>
2022-03-23 16:18:03 -0400

    step1 boilerplate

---
+++ Step1/art/art_info.py
@@ -0,0 +1,5 @@
+# TODO Fill me out
+
+FILE_NAME = ""
+ASSET_NAME = ""
+ASSET_DESCRIPTION = ""
---
+++ Step1/ipfs.py
@@ -0,0 +1,89 @@
+# Doc: https://docs.pinata.cloud/api-pinning/pin-file
+import os
+import requests
+import json
+import hashlib
+import base64
+import mimetypes
+from ipfs2bytes32 import ipfscidv0_to_byte32
+
+from secrets import (
+    PINATA_API_KEY, 
+    PINATA_API_SECRET
+)
+from art.art_info import (
+    FILE_NAME,
+    ASSET_NAME,
+    ASSET_DESCRIPTION
+)
+
+
+PINATA_IMAGE_URL = "https://api.pinata.cloud/pinning/pinFileToIPFS"
+PINATA_JSON_URL = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+PINATA_HEADERS = {
+    "pinata_api_key": PINATA_API_KEY,
+    "pinata_secret_api_key": PINATA_API_SECRET,
+}
+
+def pin_image_to_ipfs(file_path):
+    # TODO read the file in binary format
+
+    # TODO replace None with file data
+    files = {
+        'file': None,
+    }
+
+    # TODO make POST request to the correct URL, with files=files, and using the PINATA_HEADERS
+    # TODO examine response extract out the IpfsHash (this is the CID). Return it
+    pass
+
+def pin_metadata_to_ipfs(metadata):
+    # TODO make POST request to the correct URL, with json=metadata, and using the PINATA_HEADERS
+    # TODO examine response extract out the IpfsHash (this is the CID). Return it
+    pass
+
+def compute_integrity(ipfs_image_cid):
+    integrity = ipfscidv0_to_byte32(ipfs_image_cid)
+    integrity = base64.b64encode(bytes.fromhex(integrity))
+    integrity = "sha256-{}".format(integrity.decode('utf-8'))
+    return integrity
+
+def compute_metadata_hash(metadata):
+    metadata_json_string = json.dumps(metadata)
+
+    hash = hashlib.sha256()
+    hash.update(metadata_json_string.encode("utf-8"))
+    ipfs_metadata_hash = hash.digest()
+    return ipfs_metadata_hash
+
+def main():
+    # TODO compute absolute path to FILE_NAME and use it to pin image to IPFS
+    # TODO convert the IPFS CID it returns to an IPFS address
+    # TODO compute the integrity
+    # TODO compute the mimetype
+
+    metadata = {
+        'name': "",
+        'description': "",
+        'image': "",
+        'image_integrity': "",
+        'ipfs_mimetype': "",
+        'properties': {
+            'file_url': "",
+            'file_url_integrity': "",
+            'file_url_mimetype': "",
+        }
+    }
+
+    # TODO pin metadata to IPFS
+    # TODO convert the IPFS CID it returns to an IPFS address
+    # TODO compute metadata hash
+
+
+    # TODO assign to these variables to print them out
+    print("IPFS metadata CID: {}".format(ipfs_metadata_cid))
+    print("IPFS metadata address: {}".format(ipfs_metadata_address))
+    print("IPFS metadata hash: {}".format(ipfs_metadata_hash))
+
+if __name__ == '__main__':
+    main()
