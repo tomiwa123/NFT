3c06f46e8c2c8327b30ff74190fe2698aa90f21a
Kento Sugama <kentosugama@protonmail.ch>
2022-03-23 15:54:23 -0400

    update step3

--- Step3/nft_website/views.py
+++ Step3/nft_website/views.py
@@ -4,15 +4,21 @@ import requests
 # TODO USE THIS package
 # from algosdk.v2client import algod
 
-# TODO Feel free to copy-paste the secrets.py file into this folder and import it here
+# TODO Feel free to copy-paste the secrets.py file into the Step3 folder and import it here
 
+PINATA_GATEWAY = "https://gateway.pinata.cloud/ipfs/"
+
+# TODO fill me out
+ASSET_ID = -1
 
 def serve_image(request):
     # TODO modify this function to:
     # 1. Query the algorand blockchain for your NFT
-    # 2. Recover the IPFS link from the NFT
-    # 3. Query the image from IPFS
-    # 4. Serve the image as an HTTP response
+    # 2. Recover the IPFS Metadata address from the NFT
+    # 3. Query the metadata from IPFS
+    # 4. Extract the IPFS image address
+    # 5. Query the image from IPFS
+    # 6. Serve the image as an HTTP response
 
     image_bytes = open("./beautiful_temple.jpeg", "rb")
     return HttpResponse(image_bytes.read(), content_type="image/jpeg")
